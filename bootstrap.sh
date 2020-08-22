b14c55510eeed4f2b83653161a4f7fde6acfea1d
Tobias Wood <tobias@mi3.com>
2020-08-22 11:36:01 +0200

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eux
+git submodule update --init
+vcpkg/bootstrap-vcpkg.sh -allowAppleClang
+cmake -B build -S . \
+    -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake \
+    -DVCPKG_TARGET_TRIPLET=x64-osx-asan \
+    -GNinja
+cmake --build build
